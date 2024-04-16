45d6d388062710a1b3262f6221bddb1297a630e1
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-04-16 06:56:50 +0530

    own vmagent chart

--- k8sbase/monitoring-vmagent.go
+++ k8sbase/monitoring-vmagent.go
@@ -7,10 +7,13 @@ import (
 	"github.com/blesswinsamuel/infra-base/k8sapp"
 	"github.com/blesswinsamuel/infra-base/kubegogen"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 type VmagentProps struct {
-	HelmChartInfo      k8sapp.ChartInfo            `json:"helm"`
+	ImageInfo          k8sapp.ImageInfo            `json:"image"`
 	ExtraScrapeConfigs []map[string]any            `json:"extraScrapeConfigs"`
 	Resources          corev1.ResourceRequirements `json:"resources"`
 	Ingress            struct {
@@ -24,56 +27,64 @@ var vmagentConfig string
 
 // https://github.com/VictoriaMetrics/helm-charts/tree/master/charts/victoria-metrics-agent
 func (props *VmagentProps) Chart(scope kubegogen.Construct) kubegogen.Construct {
-	cprops := kubegogen.ChartProps{
-		Namespace: k8sapp.GetNamespaceContext(scope),
-	}
-	chart := scope.Chart("vmagent", cprops)
-
 	vmagentConfig := infrahelpers.FromYamlString[map[string]any](vmagentConfig)
 
-	k8sapp.NewHelm(chart, "helm", &k8sapp.HelmProps{
-		ChartInfo:   props.HelmChartInfo,
-		ReleaseName: "vmagent",
-		Namespace:   chart.Namespace(),
-		Values: map[string]any{
-			"remoteWriteUrls": []string{
-				"http://victoriametrics-victoria-metrics-single-server:8428/api/v1/write",
+	extraScrapeConfigs := []any{}
+	for _, extraScrapeConfig := range props.ExtraScrapeConfigs {
+		extraScrapeConfigs = append(extraScrapeConfigs, extraScrapeConfig)
+	}
+	vmagentConfig["scrape_configs"] = append(vmagentConfig["scrape_configs"].([]any), extraScrapeConfigs...)
+
+	app := k8sapp.NewApplicationChart(scope, "vmagent", &k8sapp.ApplicationProps{
+		Name:               "vmagent",
+		ServiceAccountName: "vmagent",
+		Containers: []k8sapp.ApplicationContainer{{
+			Name:  "vmagent",
+			Image: props.ImageInfo,
+			Ports: []k8sapp.ContainerPort{
+				{Name: "http", Port: 8429, Ingress: infrahelpers.If(props.Ingress.Enabled, &k8sapp.ApplicationIngress{Host: props.Ingress.SubDomain + "." + GetDomain(scope)}, nil)},
 			},
-			"extraScrapeConfigs": props.ExtraScrapeConfigs,
-			"resources":          props.Resources,
-			"extraArgs": map[string]any{
-				"promscrape.streamParse": "true",
+			Args: []string{
+				"-promscrape.config=/config/scrape.yml",
+				"-remoteWrite.tmpDataPath=/tmpData",
+				"-remoteWrite.url=http://victoriametrics-victoria-metrics-single-server:8428/api/v1/write",
+				"-envflag.enable=true",
+				"-envflag.prefix=VM_",
+				"-loggerFormat=json",
+				"-promscrape.streamParse=true",
 			},
-			// # extraArgs:
-			// #  http.pathPrefix: /vmagent
-			"service": map[string]any{
-				"enabled": true,
+			LivenessProbe:  &corev1.Probe{InitialDelaySeconds: 5, PeriodSeconds: 15, TimeoutSeconds: 5, ProbeHandler: corev1.ProbeHandler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString("http")}}},
+			ReadinessProbe: &corev1.Probe{InitialDelaySeconds: 5, PeriodSeconds: 15, TimeoutSeconds: 5, ProbeHandler: corev1.ProbeHandler{HTTPGet: &corev1.HTTPGetAction{Path: "/health", Port: intstr.FromString("http")}}},
+			Resources:      props.Resources,
+			ExtraVolumeMounts: []corev1.VolumeMount{
+				{Name: "tmpdata", MountPath: "/tmpdata"},
 			},
-			"ingress": map[string]any{
-				"enabled":     props.Ingress.Enabled,
-				"annotations": GetCertIssuerAnnotation(scope),
-				"hosts": []map[string]any{
-					{
-						"name": props.Ingress.SubDomain + "." + GetDomain(scope),
-						"path": "/",
-						"port": "http",
-					},
-				},
-				"tls": []map[string]any{
-					{
-						"secretName": "vmagent-tls",
-						"hosts": []string{
-							props.Ingress.SubDomain + "." + GetDomain(scope),
-						},
-					},
-				},
-				"pathType": "Prefix",
-			},
-			"config": vmagentConfig,
+		}},
+		ConfigMaps: []k8sapp.ApplicationConfigMap{
+			{Name: "vmagent", Data: map[string]string{"scrape.yml": infrahelpers.ToYamlString(vmagentConfig)}, MountPath: "/config", MountName: "config"},
+		},
+		ExtraVolumes: []corev1.Volume{
+			{Name: "tmpdata", VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}}},
 		},
 	})
-
-	return chart
+	k8sapp.NewK8sObject(app, "clusterrole", &rbacv1.ClusterRole{
+		ObjectMeta: v1.ObjectMeta{Name: "vmagent"},
+		Rules: []rbacv1.PolicyRule{
+			{APIGroups: []string{"discovery.k8s.io"}, Resources: []string{"endpointslices"}, Verbs: []string{"get", "list", "watch"}},
+			{APIGroups: []string{""}, Resources: []string{"nodes", "nodes/proxy", "nodes/metrics", "services", "endpoints", "pods"}, Verbs: []string{"get", "list", "watch"}},
+			{APIGroups: []string{"extensions", "networking.k8s.io"}, Resources: []string{"ingresses"}, Verbs: []string{"get", "list", "watch"}},
+			{NonResourceURLs: []string{"/metrics"}, Verbs: []string{"get"}},
+		},
+	})
+	k8sapp.NewK8sObject(app, "clusterrolebinding", &rbacv1.ClusterRoleBinding{
+		ObjectMeta: v1.ObjectMeta{Name: "vmagent"},
+		Subjects:   []rbacv1.Subject{{Kind: "ServiceAccount", Name: "vmagent", Namespace: app.Namespace()}},
+		RoleRef:    rbacv1.RoleRef{Kind: "ClusterRole", Name: "vmagent", APIGroup: "rbac.authorization.k8s.io"},
+	})
+	k8sapp.NewK8sObject(app, "serviceaccount", &corev1.ServiceAccount{
+		ObjectMeta: v1.ObjectMeta{Name: "vmagent"},
+	})
+	return app
 }
 
 // #     extraArgs:
