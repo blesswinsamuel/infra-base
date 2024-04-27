eae30e86e372a307d47db46aa46a8de6ec5e89f7
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-04-27 23:14:41 +0530

    remove helm dependency for traefik forward auth

--- k8sbase/auth-traefikforwardauth.go
+++ k8sbase/auth-traefikforwardauth.go
@@ -1,10 +1,15 @@
 package k8sbase
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/blesswinsamuel/infra-base/infrahelpers"
 	"github.com/blesswinsamuel/infra-base/kubegogen"
+	traefikv1alpha1 "github.com/traefik/traefik/v2/pkg/provider/kubernetes/crd/traefikio/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	"github.com/blesswinsamuel/infra-base/k8sapp"
 )
@@ -27,13 +32,13 @@ func (props *TraefikForwardAuth) Render(scope kubegogen.Scope) {
 	if props.Provider == "" {
 		props.Provider = "google"
 	}
-	env := map[string]interface{}{
+	env := map[string]string{
 		"WHITELIST":     strings.Join(props.AllowUsers, ","),
+		"TZ":            "UTC",
 		"LOG_FORMAT":    "json",
 		"LOG_LEVEL":     "info",
 		"AUTH_HOST":     props.Ingress.SubDomain + "." + GetDomain(scope),
 		"COOKIE_DOMAIN": GetDomain(scope),
-		"SECRET":        valueFromSecretKeyRef("traefik-forward-auth", "SECRET"),
 		// "PROVIDERS_GOOGLE_PROMPT": "select_account",
 	}
 	externalSecretRemoteRefs := map[string]string{
@@ -42,14 +47,10 @@ func (props *TraefikForwardAuth) Render(scope kubegogen.Scope) {
 	// https://github.com/thomseddon/traefik-forward-auth/wiki/Provider-Setup#github
 	switch props.Provider {
 	case "google":
-		env["PROVIDERS_GOOGLE_CLIENT_ID"] = valueFromSecretKeyRef("traefik-forward-auth", "PROVIDERS_GOOGLE_CLIENT_ID")
-		env["PROVIDERS_GOOGLE_CLIENT_SECRET"] = valueFromSecretKeyRef("traefik-forward-auth", "PROVIDERS_GOOGLE_CLIENT_SECRET")
 		env["DEFAULT_PROVIDER"] = "google"
 		externalSecretRemoteRefs["PROVIDERS_GOOGLE_CLIENT_SECRET"] = "FORWARD_AUTH_GOOGLE_CLIENT_SECRET"
 		externalSecretRemoteRefs["PROVIDERS_GOOGLE_CLIENT_ID"] = "FORWARD_AUTH_GOOGLE_CLIENT_ID"
 	case "github":
-		env["PROVIDERS_GENERIC_OAUTH_CLIENT_ID"] = valueFromSecretKeyRef("traefik-forward-auth", "PROVIDERS_GENERIC_OAUTH_CLIENT_ID")
-		env["PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET"] = valueFromSecretKeyRef("traefik-forward-auth", "PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET")
 		env["PROVIDERS_GENERIC_OAUTH_AUTH_URL"] = "https://github.com/login/oauth/authorize"
 		env["PROVIDERS_GENERIC_OAUTH_TOKEN_URL"] = "https://github.com/login/oauth/access_token"
 		// env["PROVIDERS_GENERIC_OAUTH_USER_URL"] = "https://api.github.com/user"
@@ -59,67 +60,34 @@ func (props *TraefikForwardAuth) Render(scope kubegogen.Scope) {
 		externalSecretRemoteRefs["PROVIDERS_GENERIC_OAUTH_CLIENT_ID"] = "FORWARD_AUTH_GITHUB_CLIENT_ID"
 		externalSecretRemoteRefs["PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET"] = "FORWARD_AUTH_GITHUB_CLIENT_SECRET"
 	}
-	// TODO: remove helm dependency
-	k8sapp.NewHelm(scope, &k8sapp.HelmProps{
-		ChartInfo:   props.HelmChartInfo,
-		ReleaseName: "traefik-forward-auth",
-		Namespace:   scope.Namespace(),
-		Values: map[string]interface{}{
-			"image": map[string]interface{}{
-				"repository": props.ImageInfo.Repository,
-				"tag":        props.ImageInfo.Tag,
+	k8sapp.NewApplication(scope, &k8sapp.ApplicationProps{
+		Name:               "traefik-forward-auth",
+		DNSPolicy:          corev1.DNSClusterFirst,
+		EnableServiceLinks: infrahelpers.Ptr(true),
+		Containers: []k8sapp.ApplicationContainer{
+			{
+				Name:             "traefik-forward-auth",
+				Image:            props.ImageInfo,
+				Ports:            []k8sapp.ContainerPort{{Name: "http", Port: 4181, Ingress: &k8sapp.ApplicationIngress{Host: props.Ingress.SubDomain + "." + GetDomain(scope)}}},
+				Args:             props.Args,
+				Env:              env,
+				EnvFromSecretRef: []string{"traefik-forward-auth"},
+				LivenessProbe:    &corev1.Probe{InitialDelaySeconds: 0, FailureThreshold: 3, PeriodSeconds: 10, TimeoutSeconds: 1, ProbeHandler: corev1.ProbeHandler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString("http")}}},
+				ReadinessProbe:   &corev1.Probe{InitialDelaySeconds: 0, FailureThreshold: 3, PeriodSeconds: 10, TimeoutSeconds: 1, ProbeHandler: corev1.ProbeHandler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString("http")}}},
+				StartupProbe:     &corev1.Probe{InitialDelaySeconds: 0, FailureThreshold: 30, PeriodSeconds: 5, TimeoutSeconds: 1, ProbeHandler: corev1.ProbeHandler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString("http")}}},
 			},
-			"controller": map[string]interface{}{
-				"annotations": map[string]interface{}{
-					"secret.reloader.stakater.com/reload": "traefik-forward-auth",
-				},
-			},
-			"ingress": map[string]interface{}{
-				"main": map[string]interface{}{
-					"enabled": true,
-					"annotations": infrahelpers.MergeAnnotations(
-						GetCertIssuerAnnotation(scope),
-						map[string]string{"traefik.ingress.kubernetes.io/router.middlewares": "auth-traefik-forward-auth@kubernetescrd"},
-					),
-					"hosts": []map[string]interface{}{
-						{
-							"host": props.Ingress.SubDomain + "." + GetDomain(scope),
-							"paths": []map[string]interface{}{
-								{"path": "/"},
-							},
-						},
-					},
-					"tls": []map[string]interface{}{
-						{
-							"hosts": []string{
-								props.Ingress.SubDomain + "." + GetDomain(scope),
-							},
-							"secretName": "traefik-forward-auth-tls",
-						},
-					},
-				},
-			},
-			"args": props.Args,
-			"env":  env,
-			"middleware": map[string]interface{}{
-				"nameOverride": "traefik-forward-auth",
+		},
+		ExternalSecrets: []k8sapp.ApplicationExternalSecret{
+			{Name: "traefik-forward-auth", RemoteRefs: externalSecretRemoteRefs},
+		},
+	})
+	scope.AddApiObject(&traefikv1alpha1.Middleware{
+		ObjectMeta: metav1.ObjectMeta{Name: "traefik-forward-auth"},
+		Spec: traefikv1alpha1.MiddlewareSpec{
+			ForwardAuth: &traefikv1alpha1.ForwardAuth{
+				Address:             fmt.Sprintf("http://%s.%s:4181", "traefik-forward-auth", scope.Namespace()),
+				AuthResponseHeaders: []string{"X-Forwarded-User"},
 			},
 		},
 	})
-
-	k8sapp.NewExternalSecret(scope, &k8sapp.ExternalSecretProps{
-		Name:       "traefik-forward-auth",
-		RemoteRefs: externalSecretRemoteRefs,
-	})
-}
-
-func valueFromSecretKeyRef(name string, key string) map[string]interface{} {
-	return map[string]interface{}{
-		"valueFrom": map[string]interface{}{
-			"secretKeyRef": map[string]interface{}{
-				"name": name,
-				"key":  key,
-			},
-		},
-	}
 }
