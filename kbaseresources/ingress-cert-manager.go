195bbaf6c4ab0784b36d0b3b607287c0cf281427
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-08-09 23:06:19 +0530

    use public dns for cert manager

--- kbaseresources/ingress-cert-manager.go
+++ kbaseresources/ingress-cert-manager.go
@@ -12,14 +12,21 @@ type CertManagerProps struct {
 // https://github.com/cert-manager/cert-manager/tree/master/deploy/charts/cert-manager
 // https://artifacthub.io/packages/helm/cert-manager/cert-manager
 func (props *CertManagerProps) Render(scope kgen.Scope) {
+	values := map[string]interface{}{
+		"installCRDs": "true",
+		"extraArgs": []string{
+			"--enable-certificate-owner-ref=true",
+		},
+		"dns01RecursiveNameservers":     "8.8.8.8:53,1.1.1.1:53",
+		"dns01RecursiveNameserversOnly": true,
+		// "podDnsPolicy":                  "None",
+		// "podDnsConfig": map[string]interface{}{
+		// 	"nameservers": []string{"1.1.1.1", "8.8.8.8"},
+		// },
+	}
 	k8sapp.NewHelm(scope, &k8sapp.HelmProps{
 		ChartInfo:   props.HelmChartInfo,
 		ReleaseName: "cert-manager",
-		Values: map[string]interface{}{
-			"installCRDs": "true",
-			"extraArgs": []string{
-				"--enable-certificate-owner-ref=true",
-			},
-		},
+		Values:      values,
 	})
 }
