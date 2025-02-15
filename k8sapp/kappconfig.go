734796ea7f686a1d19b4afc969a2db18c88ce9cd
Blesswin Samuel <git@bless.win>
2025-02-15 06:09:58 +0530

    add resource matchers for improved configuration handling in kapp

--- k8sapp/kappconfig.go
+++ k8sapp/kappconfig.go
@@ -2,17 +2,79 @@ package k8sapp
 
 import "github.com/blesswinsamuel/kgen"
 
+// https://github.com/carvel-dev/kapp/blob/develop/pkg/kapp/config/resource_matchers.go
+type ResourceMatchers []ResourceMatcher
+
+type ResourceMatcher struct {
+	AllMatcher               *AllMatcher               `json:"allMatcher,omitempty"`
+	AnyMatcher               *AnyMatcher               `json:"anyMatcher,omitempty"`
+	NotMatcher               *NotMatcher               `json:"notMatcher,omitempty"`
+	AndMatcher               *AndMatcher               `json:"andMatcher,omitempty"`
+	APIGroupKindMatcher      *APIGroupKindMatcher      `json:"apiGroupKindMatcher,omitempty"`
+	APIVersionKindMatcher    *APIVersionKindMatcher    `json:"apiVersionKindMatcher,omitempty"`
+	KindNamespaceNameMatcher *KindNamespaceNameMatcher `json:"kindNamespaceNameMatcher,omitempty"`
+	HasAnnotationMatcher     *HasAnnotationMatcher     `json:"hasAnnotationMatcher,omitempty"`
+	HasNamespaceMatcher      *HasNamespaceMatcher      `json:"hasNamespaceMatcher,omitempty"`
+	CustomResourceMatcher    *CustomResourceMatcher    `json:"customResourceMatcher,omitempty"`
+	EmptyFieldMatcher        *EmptyFieldMatcher        `json:"emptyFieldMatcher,omitempty"`
+}
+
+type AllMatcher struct{}
+
+type AnyMatcher struct {
+	Matchers []ResourceMatcher `json:"matchers,omitempty"`
+}
+
+type NotMatcher struct {
+	Matcher ResourceMatcher `json:"matcher,omitempty"`
+}
+
+type AndMatcher struct {
+	Matchers []ResourceMatcher `json:"matchers,omitempty"`
+}
+
+type APIGroupKindMatcher struct {
+	APIGroup string `json:"apiGroup,omitempty"`
+	Kind     string `json:"kind,omitempty"`
+}
+
+type APIVersionKindMatcher struct {
+	APIVersion string `json:"apiVersion,omitempty"`
+	Kind       string `json:"kind,omitempty"`
+}
+
+type KindNamespaceNameMatcher struct {
+	Kind      string `json:"kind,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+	Name      string `json:"name,omitempty"`
+}
+
+type HasAnnotationMatcher struct {
+	Keys []string `json:"keys,omitempty"`
+}
+
+type HasNamespaceMatcher struct {
+	Names []string `json:"names,omitempty"`
+}
+
+type CustomResourceMatcher struct{}
+
+type EmptyFieldMatcher struct {
+	Path string `json:"path,omitempty"`
+	// Path ctlres.Path
+}
+
 func NewKappConfig(scope kgen.Scope) kgen.Scope {
 	// https://carvel.dev/kapp/docs/v0.45.0/config/#rebaserules
 	scope = scope.CreateScope("kapp-config", kgen.ScopeProps{})
 
 	// https://github.com/carvel-dev/kapp/blob/2d0b7edbcd49a58263a37c48c5a614704c0d091f/pkg/kapp/config/resource_matchers.go#L12
 	// https://carvel.dev/kapp/docs/v0.62.x/rebase-pvc/
-	clusterOwnedFields := func(paths [][]string, resourceMatchers []map[string]any) map[string]any {
+	clusterOwnedFields := func(paths [][]string, resourceMatchers any) map[string]any {
 		return map[string]any{
 			"paths":            paths,
 			"type":             "copy",
-			"sources":          []any{"new", "existing"},
+			"sources":          []any{"existing", "new"},
 			"resourceMatchers": resourceMatchers,
 		}
 	}
@@ -23,19 +85,22 @@ func NewKappConfig(scope kgen.Scope) kgen.Scope {
 		"rebaseRules": []any{
 			// https://github.com/carvel-dev/kapp/issues/49
 			// https://gist.github.com/cppforlife/149872f132d6afdc6f0240d70f598a16
-			clusterOwnedFields([][]string{{"data"}}, []map[string]any{
-				{"kindNamespaceNameMatcher": map[string]any{"kind": "Secret", "namespace": "external-secrets", "name": "external-secrets-webhook"}},
-				{"kindNamespaceNameMatcher": map[string]any{"kind": "Secret", "namespace": "secrets", "name": "external-secrets-webhook"}},
+			clusterOwnedFields([][]string{{"data"}}, []ResourceMatcher{
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "ingress", Name: "cilium-ca"}},
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "ingress", Name: "hubble-server-certs"}},
 
-				{"kindNamespaceNameMatcher": map[string]any{"kind": "Secret", "namespace": "metallb", "name": "metallb-webhook-cert"}},
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "external-secrets", Name: "external-secrets-webhook"}},
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "secrets", Name: "external-secrets-webhook"}},
 
-				{"kindNamespaceNameMatcher": map[string]any{"kind": "Secret", "namespace": "system", "name": "kubernetes-dashboard-csrf"}},
-				{"kindNamespaceNameMatcher": map[string]any{"kind": "Secret", "namespace": "system", "name": "kubernetes-dashboard-key-holder"}},
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "metallb", Name: "metallb-webhook-cert"}},
+
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "system", Name: "kubernetes-dashboard-csrf"}},
+				{KindNamespaceNameMatcher: &KindNamespaceNameMatcher{Kind: "Secret", Namespace: "system", Name: "kubernetes-dashboard-key-holder"}},
 			}),
-			clusterOwnedFields([][]string{{"spec", "template", "metadata", "annotations", "reloader.stakater.com/last-reloaded-from"}}, []map[string]any{
-				{"apiVersionKindMatcher": map[string]any{"apiVersion": "apps/v1", "kind": "Deployment"}},
-				{"apiVersionKindMatcher": map[string]any{"apiVersion": "apps/v1", "kind": "StatefulSet"}},
-				{"apiVersionKindMatcher": map[string]any{"apiVersion": "apps/v1", "kind": "Daemonset"}},
+			clusterOwnedFields([][]string{{"spec", "template", "metadata", "annotations", "reloader.stakater.com/last-reloaded-from"}}, []ResourceMatcher{
+				{APIVersionKindMatcher: &APIVersionKindMatcher{APIVersion: "apps/v1", Kind: "Deployment"}},
+				{APIVersionKindMatcher: &APIVersionKindMatcher{APIVersion: "apps/v1", Kind: "StatefulSet"}},
+				{APIVersionKindMatcher: &APIVersionKindMatcher{APIVersion: "apps/v1", Kind: "Daemonset"}},
 			}),
 			clusterOwnedFields([][]string{{"metadata", "annotations", "force-resync"}}, []map[string]any{
 				{"apiVersionKindMatcher": map[string]any{"apiVersion": "external-secrets.io/v1beta1", "kind": "ExternalSecret"}},
