d7398c6659a0bf1b6d305b16f00f15c71c454131
Blesswin Samuel <git@bless.win>
2025-02-15 06:10:07 +0530

    add routing configuration for Cilium ingress

--- kbaseresources/ingress-cilium.go
+++ kbaseresources/ingress-cilium.go
@@ -21,29 +21,38 @@ func (props *CiliumProps) Render(scope kgen.Scope) {
 		ReleaseName: "cilium",
 		Values: map[string]any{
 			// "installCRDs": "true",
+			"routingMode":           "native",
+			"ipv4NativeRoutingCIDR": "10.0.0.0/8",
+			"autoDirectNodeRoutes":  true,
+			"ipam": map[string]any{
+				"mode": "kubernetes",
+				"operator": map[string]any{
+					"clusterPoolIPv4PodCIDRList": []string{"10.0.0.0/8"},
+				},
+			},
 			"operator": map[string]any{
 				"replicas": 1,
 			},
-			"hubble": map[string]any{
-				"tls": map[string]any{
-					"enabled": false,
-					"auto": map[string]any{
-						"method": "cronJob",
-						// "method": "certmanager",
-						// "certManagerIssuerRef": map[string]any{
-						// 	"group": "cert-manager.io",
-						// 	"kind":  k8sapp.GetGlobals(scope).Cert.CertIssuerKind,
-						// 	"name":  k8sapp.GetGlobals(scope).Cert.CertIssuerName,
-						// },
-					},
-				},
-				"relay": map[string]any{
-					"enabled": true,
-				},
-				"ui": map[string]any{
-					"enabled": true,
-				},
-			},
+			// "hubble": map[string]any{
+			// 	"tls": map[string]any{
+			// 		"enabled": false,
+			// 		"auto": map[string]any{
+			// 			"method": "cronJob",
+			// 			// "method": "certmanager",
+			// 			// "certManagerIssuerRef": map[string]any{
+			// 			// 	"group": "cert-manager.io",
+			// 			// 	"kind":  k8sapp.GetGlobals(scope).Cert.CertIssuerKind,
+			// 			// 	"name":  k8sapp.GetGlobals(scope).Cert.CertIssuerName,
+			// 			// },
+			// 		},
+			// 	},
+			// 	"relay": map[string]any{
+			// 		"enabled": true,
+			// 	},
+			// 	"ui": map[string]any{
+			// 		"enabled": true,
+			// 	},
+			// },
 			// "secretsBackend": map[string]any{
 			// 	"secretsBackend": "k8s",
 			// },
